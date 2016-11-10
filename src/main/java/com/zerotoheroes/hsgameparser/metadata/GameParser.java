5d4b611d5d10e8796e0b96d089d826fc144f14d5
sebastientromp <sebastien.tromp@gmail.com>
2016-11-10 19:54:32 +0100

    Expose cards list (static field)

--- src/main/java/com/zerotoheroes/hsgameparser/metadata/GameParser.java
+++ src/main/java/com/zerotoheroes/hsgameparser/metadata/GameParser.java
@@ -25,9 +25,9 @@ import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@Getter
 public class GameParser {
 
+	@Getter
 	private static CardsList cardsList;
 
 	public GameParser() throws Exception {
