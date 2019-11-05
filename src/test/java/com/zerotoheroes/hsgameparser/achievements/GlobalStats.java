15d5ad6f9f8d0c5af0f83ad96c3b396b59605180
sebastientromp <sebastien.tromp@gmail.com>
2019-11-05 14:23:13 +0100

    add more Global Stat achievement

--- src/test/java/com/zerotoheroes/hsgameparser/achievements/GlobalStats.java
+++ src/test/java/com/zerotoheroes/hsgameparser/achievements/GlobalStats.java
@@ -4,6 +4,9 @@ public class GlobalStats {
 
     public static class Key {
         public static final String TOTAL_DAMAGE_TO_ENEMY_HERO = "total-damage-to-enemy-hero";
+        public static final String TOTAL_MANA_SPENT = "total-mana-spent";
+        public static final String TOTAL_ENEMY_MINIONS_DEATH = "total-enemy-minions-death";
+        public static final String TOTAL_DURATION = "total-duration";
     }
 
     public static class Context {
--- src/test/java/com/zerotoheroes/hsgameparser/achievements/generator/GlobalAchievements.java
+++ src/test/java/com/zerotoheroes/hsgameparser/achievements/generator/GlobalAchievements.java
@@ -34,11 +34,14 @@ public class GlobalAchievements implements WithAssertions {
 
     private List<RawAchievement> buildTrackingAchievements() throws Exception {
         List<RawAchievement> damageDealtToEnemyHeroes = buildDamageDealtToEnemyHeroes();
-        List<RawAchievement> result = Stream.of(damageDealtToEnemyHeroes)
+        List<RawAchievement> manaSpents = buildManaSpents();
+        List<RawAchievement> enemyMinionsDeads = buildEnemyMinionsDeads();
+        List<RawAchievement> totalDurations = buildTotalDurations();
+        List<RawAchievement> result = Stream.of(damageDealtToEnemyHeroes, manaSpents, enemyMinionsDeads, totalDurations)
                 .flatMap(List::stream)
                 .sorted(Comparator.comparing(RawAchievement::getId))
                 .collect(Collectors.toList());
-        assertThat(result.size()).isEqualTo(7);
+        assertThat(result.size()).isEqualTo(35);
         List<String> types = result.stream()
                 .map(RawAchievement::getType)
                 .map(type -> "'" + type + "'")
@@ -49,7 +52,7 @@ public class GlobalAchievements implements WithAssertions {
     }
 
     private List<RawAchievement> buildDamageDealtToEnemyHeroes() throws Exception {
-        List<Integer> targetDamages = newArrayList(200, 1_000, 5_000, 10_000, 50_000, 100_000, 500_000);
+        List<Integer> targetDamages = newArrayList(200, 1_000, 5_000, 10_000, 50_000, 100_000, 500_000, 1_000_000);
         return targetDamages.stream()
                 .map(targetDamage -> buildDamageDealtToEnemyHero(targetDamage, targetDamage == 200))
                 .collect(Collectors.toList());
@@ -69,7 +72,11 @@ public class GlobalAchievements implements WithAssertions {
                 .displayCardId("DALA_BOSS_24e")
                 .displayCardType("minion")
                 .difficulty("rare")
-                .text("Deal damage to enemy heroes (%%globalStats.total-damage-to-enemy-hero.global%% damage dealt so far)")
+                .text("Deal damage to enemy heroes in all game modes (%%globalStats."
+                        + GlobalStats.Key.TOTAL_DAMAGE_TO_ENEMY_HERO
+                        + "."
+                        + GlobalStats.Context.GLOBAL
+                        + "%% damage dealt so far)")
                 .completedText("Dealt " + formatter.format(targetDamage) + " damage to enemy heroes")
                 .maxNumberOfRecords(1)
                 .points(25)
@@ -78,6 +85,123 @@ public class GlobalAchievements implements WithAssertions {
                                 GlobalStats.Key.TOTAL_DAMAGE_TO_ENEMY_HERO,
                                 GlobalStats.Context.GLOBAL,
                                 "" + targetDamage)
+                        ).build()
+                ))
+                .build();
+    }
+
+    private List<RawAchievement> buildManaSpents() throws Exception {
+        List<Integer> targetManas = newArrayList(200, 1_000, 5_000, 10_000, 50_000, 100_000, 500_000, 1_000_000);
+        return targetManas.stream()
+                .map(targetMana -> buildManaSpent(targetMana, targetMana == 200))
+                .collect(Collectors.toList());
+    }
+
+    private RawAchievement buildManaSpent(int targetMana, boolean isRoot) {
+        DecimalFormat formatter = (DecimalFormat) NumberFormat.getInstance(Locale.US);
+        return RawAchievement.builder()
+                .id("global_mana_spent_" + targetMana)
+                .type("global_mana_spent")
+                .icon("boss_victory")
+                .root(isRoot)
+                .canBeCompletedOnlyOnce(true)
+                .priority(targetMana)
+                .name("Mana Charged")
+                .displayName("Achievement completed: Mana Charged (" + formatter.format(targetMana) + " mana crystals spent)")
+                .displayCardId("CS2_013t")
+                .displayCardType("minion")
+                .difficulty("rare")
+                .text("Spend mana crystals in all game modes (%%globalStats."
+                        + GlobalStats.Key.TOTAL_MANA_SPENT
+                        + "."
+                        + GlobalStats.Context.GLOBAL
+                        + "%% mana spent so far)")
+                .completedText("Spent " + formatter.format(targetMana) + " mana crystals")
+                .maxNumberOfRecords(1)
+                .points(25)
+                .requirements(newArrayList(
+                        Requirement.builder().type(GLOBAL_STAT).values(newArrayList(
+                                GlobalStats.Key.TOTAL_MANA_SPENT,
+                                GlobalStats.Context.GLOBAL,
+                                "" + targetMana)
+                        ).build()
+                ))
+                .build();
+    }
+
+    private List<RawAchievement> buildEnemyMinionsDeads() throws Exception {
+        List<Integer> targetMinionsDead = newArrayList(50, 100, 200, 500, 1_000, 2_000, 5_000, 10_000, 50_000, 100_000);
+        return targetMinionsDead.stream()
+                .map(minionsDead -> buildEnemyMinionsDead(minionsDead, minionsDead == 50))
+                .collect(Collectors.toList());
+    }
+
+    private RawAchievement buildEnemyMinionsDead(int minionsDead, boolean isRoot) {
+        DecimalFormat formatter = (DecimalFormat) NumberFormat.getInstance(Locale.US);
+        return RawAchievement.builder()
+                .id("global_enemy_minions_dead_" + minionsDead)
+                .type("global_enemy_minions_dead")
+                .icon("boss_victory")
+                .root(isRoot)
+                .canBeCompletedOnlyOnce(true)
+                .priority(minionsDead)
+                .name("Assassin")
+                .displayName("Achievement completed: Assassin (" + formatter.format(minionsDead) + " enemy minions dead)")
+                .displayCardId("CS2_076")
+                .displayCardType("spell")
+                .difficulty("rare")
+                .text("Kill enemy minions in all game modes (%%globalStats."
+                        + GlobalStats.Key.TOTAL_ENEMY_MINIONS_DEATH
+                        + "."
+                        + GlobalStats.Context.GLOBAL
+                        + "%% minions killed so far)")
+                .completedText("Killed " + formatter.format(minionsDead) + " enemy minions")
+                .maxNumberOfRecords(1)
+                .points(25)
+                .requirements(newArrayList(
+                        Requirement.builder().type(GLOBAL_STAT).values(newArrayList(
+                                GlobalStats.Key.TOTAL_ENEMY_MINIONS_DEATH,
+                                GlobalStats.Context.GLOBAL,
+                                "" + minionsDead)
+                        ).build()
+                ))
+                .build();
+    }
+
+    private List<RawAchievement> buildTotalDurations() throws Exception {
+        List<Integer> targetDurations = newArrayList(1, 10, 20, 50, 100, 200, 300, 400, 500);
+        return targetDurations.stream()
+                .map(targetDuration -> buildTotalDuration(targetDuration, targetDuration == 1))
+                .collect(Collectors.toList());
+    }
+
+    private RawAchievement buildTotalDuration(int hoursPlayed, boolean isRoot) {
+        DecimalFormat formatter = (DecimalFormat) NumberFormat.getInstance(Locale.US);
+        return RawAchievement.builder()
+                .id("global_total_duration_" + hoursPlayed)
+                .type("global_total_duration")
+                .icon("boss_victory")
+                .root(isRoot)
+                .canBeCompletedOnlyOnce(true)
+                .priority(hoursPlayed)
+                .name("Addicted")
+                .displayName("Achievement completed: Addicted (" + formatter.format(hoursPlayed) + " hours spent in matches)")
+                .displayCardId("UNG_028t")
+                .displayCardType("spell")
+                .difficulty("rare")
+                .text("Spend time in matches in all game modes (%%globalStats."
+                        + GlobalStats.Key.TOTAL_DURATION
+                        + "."
+                        + GlobalStats.Context.GLOBAL
+                        + "%% hours spent in matches so far)")
+                .completedText("Spent " + formatter.format(hoursPlayed) + " hours in matches")
+                .maxNumberOfRecords(1)
+                .points(25)
+                .requirements(newArrayList(
+                        Requirement.builder().type(GLOBAL_STAT).values(newArrayList(
+                                GlobalStats.Key.TOTAL_DURATION,
+                                GlobalStats.Context.GLOBAL,
+                                "" + hoursPlayed)
                         ).build()
                 ))
                 .build();
